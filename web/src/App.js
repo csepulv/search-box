324c50a78f391ee0c60229f5b5731128f66faa8f
Christian Sepulveda <csepulv@gmail.com>
2017-01-15 12:48:16 -0800

    very simple search box

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {TextField, List, ListItem, Divider} from 'material-ui'
+
+import crayola from '../resources/crayola.json'
+
+// (Make material-ui happy)
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {colors: [], query: ''};
+    }
+
+    search = () => {
+        const results = crayola.filter(color => color.name.toLowerCase().includes(this.state.query.toLowerCase()));
+        this.setState({colors: results});
+    };
+
+    handleKeyDown = (event) => {
+        const ENTER_KEY = 13;
+        if (event.keyCode === ENTER_KEY) {
+            event.preventDefault();
+            this.search();
+        }
+    };
+    handleQueryChange = (event, value) => {
+        this.setState({query: value});
+    };
+
+    render() {
+        const listItems = this.state.colors.map((color, index) => {
+            return (
+                <div>
+                    <ListItem key={`color-${index}`} primaryText={color.name} style={{backgroundColor: color.hex} }/>
+                    <Divider/>
+                </div>
+            );
+        });
+        return (
+            <MuiThemeProvider>
+                <div>
+                    <TextField hintText="Search..."
+                               floatingLabelFixed={true}
+                               fullWidth={true}
+                               value={this.state.query}
+                               onChange={this.handleQueryChange}
+                               onKeyDown={this.handleKeyDown}/>
+                    <List>
+                        {listItems}
+                    </List>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
