58216af4c98b81633396d1acb4c9f577ac261e10
Christian Sepulveda <csepulv@gmail.com>
2017-01-15 19:46:58 -0800

    fix broken tests (starter/bootstrap smoke tests)

--- mobile/__tests__/index.ios.js
+++ mobile/__tests__/index.ios.js
@@ -1,12 +1,20 @@
 import 'react-native';
 import React from 'react';
+
+
+// need following to fix
+// Invariant Violation: ReactUpdates: must inject a reconcile transaction class and batching strategy
+// https://wietse.loves.engineering/using-jest-with-react-native-and-mobx-34949ea7d2cf#.719t4c63f
+jest.mock('mobx-react/native', () => require('mobx-react/custom'));
+
+
 import Index from '../index.ios.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  );
+    const tree = renderer.create(
+        <Index />
+    );
 });
--- web/src/App.test.js
+++ web/src/App.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import Colors from './colors'
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App colors={new Colors()} />, div);
 });
